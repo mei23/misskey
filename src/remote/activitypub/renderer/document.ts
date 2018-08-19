062fd5b4527a28a5d32ce53354159691237d33bb
mei23 <m@m544.net>
2018-08-19 18:01:28 +0900

    revert documet

--- src/remote/activitypub/renderer/document.ts
+++ src/remote/activitypub/renderer/document.ts
@@ -4,6 +4,5 @@ import { IDriveFile } from '../../../models/drive-file';
 export default (file: IDriveFile) => ({
 	type: 'Document',
 	mediaType: file.contentType,
-	url: file.metadata.url || `${config.drive_url}/${file._id}`,
-	sensitive: file.metadata.isSensitive
+	url: file.metadata.url || `${config.drive_url}/${file._id}`
 });
