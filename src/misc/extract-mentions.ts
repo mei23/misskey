c294fe2ddf40faf691d71f996a2a5decb02f1cdf
mei23 <m@m544.net>
2021-04-04 18:34:38 +0900

    fix test

--- src/misc/extract-mentions.ts
+++ src/misc/extract-mentions.ts
@@ -2,6 +2,10 @@
 
 import { MfmNode, MfmMentionNode } from '../mfm/prelude';
 
+/**
+ * Extract mentions
+ * @param nodes parseBasicの結果を入れる
+ */
 export default function(nodes: MfmNode[]): MfmMentionNode['props'][] {
 	// TODO: 重複を削除
 	const mentionNodes = nodes.filter(x => x.type === 'mention') as MfmMentionNode [];
