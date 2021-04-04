079aae3a8f215d41bccd41e72056a8db0b77fad7
mei23 <m@m544.net>
2021-04-04 21:31:34 +0900

    コメント

--- src/mfm/prelude.ts
+++ src/mfm/prelude.ts
@@ -1,30 +1,46 @@
 export interface MfmNode {
+	/** Node type */
 	type: string;
+	/** Properties */
 	props: Record<string, any>;
+	/** Child nodes */
 	children: MfmNode[];
 }
 
+/** メンション */
 export interface MfmMentionNode extends MfmNode {
 	type: 'mention';
 	props: {
-		canonical: string;
 		username: string;
-		host: string;
+		host: string | null;
+		/** 入力された表記 */
 		acct: string;
+		/** ローカルホストの場合省略される, punycodeはデコードされる 表記 */
+		canonical: string;
 	}
 }
 
+/** ハッシュタグ */
 export interface MfmHashtagNode extends MfmNode {
 	type: 'hashtag';
 	props: {
+		/** ハッシュタグ (#は含まない) */
 		hashtag: string;
 	};
 }
 
+/** 絵文字 */
 export interface MfmEmojiNode extends MfmNode {
 	type: 'emoji';
 	props: {
-		name: string;
+		/** カスタム絵文字の場合 カスタム絵文字名 (:は含まない) */
+		name?: string;
+		/** Unicode絵文字の場合 絵文字 */
+		emoji?: string;
+		/** Unicode絵文字の場合 Twemojiを迂回するか */
+		vendor?: boolean;
+		/** Unicode絵文字の場合 ローカル定義の絵文字か */
+		local?: boolean;
 	};
 }
 
