407940fef82ad2e9c73d075dd219fbefb1acf662
mei23 <m@m544.net>
2022-01-29 05:52:52 +0900

    t5

--- src/remote/activitypub/resolver.ts
+++ src/remote/activitypub/resolver.ts
@@ -60,8 +60,6 @@ export default class Resolver {
 			this.user = await getInstanceActor();
 		}
 
-		console.log(`resolve: ${value}`);
-
 		const object = this.user
 			? await signedGet(value, this.user)
 			: await getJson(value, 'application/activity+json, application/ld+json');
