2063634d98e7bfd9f769b17408374ef96f87285e
mei23 <m@m544.net>
2022-02-25 07:01:56 +0900

    wip

--- src/misc/emoji-regex.ts
+++ src/misc/emoji-regex.ts
@@ -6,3 +6,7 @@ export const emojiRegexWithCustom = new RegExp(`(${emojiRegex.source}|:[0-9A-Za-
 
 // Ninja catなどTwemojiを迂回させたいもの
 export const vendorEmojiRegex = /(\uD83D\uDC31\u200D(?:\uD83D\uDC64|\uD83D\uDE80|\uD83D\uDC53|\uD83D\uDCBB|\uD83D\uDC09|\uD83C\uDFCD))/;
+
+// ローカル定義Twemoji
+export const localEmojiRegex = /(\uD83E[\uDEE0-\uDEE6]|\uD83E\uDD79|\uD83E[\uDEF0-\uDEF6]|\uD83E[\uDEC3-\uDEC5])/;
+
