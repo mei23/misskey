e19a25fa8770cf3787f2f6c70e5766b56d257e20
mei23 <m@m544.net>
2022-06-11 19:45:51 +0900

    wip

--- src/client/app/desktop/views/components/drive.vue
+++ src/client/app/desktop/views/components/drive.vue
@@ -22,22 +22,17 @@
 	>
 		<div class="selection" ref="selection"></div>
 		<div class="contents" ref="contents">
-			<div class="folders" ref="foldersContainer" v-if="folders.length > 0 || moreFolders">
+			<div class="folders" ref="foldersContainer">
 				<x-folder v-for="folder in folders" :key="folder.id" class="folder" :folder="folder"/>
-				<!-- SEE: https://stackoverflow.com/questions/18744164/flex-box-align-last-row-to-grid -->
-				<div class="padding" v-for="n in 16"></div>
 				<ui-button v-if="moreFolders">{{ $t('@.load-more') }}</ui-button>
 			</div>
 			<div class="files" ref="filesContainer" v-if="files.length > 0 || moreFiles">
 				<x-file v-for="file in files" :key="file.id" class="file" :file="file"/>
-				<!-- SEE: https://stackoverflow.com/questions/18744164/flex-box-align-last-row-to-grid -->
-				<div class="padding" v-for="n in 16"></div>
 				<ui-button v-if="moreFiles" @click="fetchMoreFiles">{{ $t('@.load-more') }}</ui-button>
 			</div>
-			<div class="empty" v-if="files.length == 0 && !moreFiles && folders.length == 0 && !moreFolders && !fetching">
+			<div class="files" v-else-if="!fetching">
 				<p v-if="draghover">{{ $t('empty-draghover') }}</p>
-				<p v-if="!draghover && folder == null"><strong>{{ $t('empty-drive') }}</strong><br/>{{ $t('empty-drive-description') }}</p>
-				<p v-if="!draghover && folder != null">{{ $t('empty-folder') }}</p>
+				<p v-else>{{ $t('empty-folder') }}</p>
 			</div>
 		</div>
 		<div class="fetching" v-if="fetching">
@@ -641,7 +636,6 @@ export default Vue.extend({
 						margin 0
 
 	> .main
-		padding 8px
 		height calc(100% - 38px)
 		overflow auto
 		background var(--desktopDriveBg)
@@ -670,17 +664,31 @@ export default Vue.extend({
 			border solid 1px var(--primary)
 			background var(--primaryAlpha05)
 			pointer-events none
-
+ 
 		> .contents
+			display flex
+			height 100%
+			overflow hidden
 
 			> .folders
+				display flex
+				flex-direction column
+				overflow-y auto
+				overflow-x hidden
+				width 200px
+				padding 8px
+
 			> .files
 				display flex
 				flex-wrap wrap
+				overflow-y auto
+				width 100%
+				padding 8px
 
+			> .folders
+			> .files
 				> .folder
 				> .file
-					flex-grow 1
 					width 144px
 					margin 4px
 
@@ -688,7 +696,6 @@ export default Vue.extend({
 					flex-grow 1
 					pointer-events none
 					width 144px + 8px // 8px is margin
-
 			> .empty
 				padding 16px
 				text-align center
