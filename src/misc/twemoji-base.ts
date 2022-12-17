b8e81075e3925df1a73a5f8e2bf26e1fddf86a38
mei23 <m@m544.net>
2022-12-18 01:51:17 +0900

    twemojiSvgBase

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -2,7 +2,9 @@
 // export const twemojiSvgBase = 'https://twemoji.maxcdn.com/v/latest/svg';
 
 // Alternative (Fast, with IPv6)
-export const twemojiSvgBase = 'https://cdn.jsdelivr.net/npm/@discordapp/twemoji@14.0.2/dist/svg';
+//export const twemojiSvgBase = 'https://cdn.jsdelivr.net/npm/@discordapp/twemoji@14.0.2/dist/svg';
+
+export const twemojiSvgBase = 'https://raw.githubusercontent.com/mei23/memoji/4e18e3d03d8c83b6d66c6f80593aba7fb3d00bc0/dist/svg';
 
 // Self hosting
 //export const twemojiSvgBase = '/twemoji';
