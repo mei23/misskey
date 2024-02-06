438248f95aa1d81336a960e684c1ce26f8ea23ef
mei23 <m@m544.net>
2024-02-06 09:11:45 +0900

    あ

--- src/remote/activitypub/renderer/index.ts
+++ src/remote/activitypub/renderer/index.ts
@@ -11,41 +11,112 @@ export const renderActivity = (x: any): IActivity | null => {
 		x.id = `${config.url}/${uuid()}`;
 	}
 
-	return Object.assign({
-		'@context': [
-			'https://www.w3.org/ns/activitystreams',
-			'https://w3id.org/security/v1',
-			{
-				Key: 'sec:Key',
-				// as non-standards
-				manuallyApprovesFollowers: 'as:manuallyApprovesFollowers',
-				sensitive: 'as:sensitive',
-				Hashtag: 'as:Hashtag',
-				// Mastodon
-				toot: 'http://joinmastodon.org/ns#',
-				Emoji: 'toot:Emoji',
-				featured: 'toot:featured',
-				discoverable: 'toot:discoverable',
-				indexable: 'toot:indexable',
-				// schema
-				schema: 'http://schema.org#',
-				PropertyValue: 'schema:PropertyValue',
-				value: 'schema:value',
-				// Misskey
-				misskey: 'https://misskey-hub.net/ns#',
-				'_misskey_content': 'misskey:_misskey_content',
-				'_misskey_quote': 'misskey:_misskey_quote',
-				'_misskey_reaction': 'misskey:_misskey_reaction',
-				'_misskey_votes': 'misskey:_misskey_votes',
-				'isCat': 'misskey:isCat',
-				// vcard
-				vcard: 'http://www.w3.org/2006/vcard/ns#',
-				// Fedibird
-				fedibird: 'http://fedibird.com/ns#',
-				quoteUri: 'fedibird:quoteUri',
-				searchableBy: { '@id': 'fedibird:searchableBy', '@type': '@id' },
+	const imports = [
+		'https://www.w3.org/ns/activitystreams',
+		'https://w3id.org/security/v1',
+	];
+
+	// namespaces (null means already defined in imports)
+	const nss: Record<string, string | null> = {
+		as: null,	// defined in 'https://www.w3.org/ns/activitystreams'
+		vcard: null,	// defined in 'https://www.w3.org/ns/activitystreams'
+		sec: null,	// defined in 'https://w3id.org/security/v1'
+		// :
+
+		// Some extra or vendor namespaces
+		toot: 'http://joinmastodon.org/ns#',	// So-called Mastodon
+		schema: 'http://schema.org#',
+		misskey: 'https://misskey-hub.net/ns#',
+		fedibird: 'http://fedibird.com/ns#',
+	};
+
+	// Definitions! (required ns => key => value)
+	const defs: Record<string, Record<string, string | object>> = {
+		sec: {
+			Key: 'sec:Key',
+		},
+		as: {	// Not defined by the activitystreams, but defined in Mastodon.
+			manuallyApprovesFollowers: 'as:manuallyApprovesFollowers',
+			sensitive: 'as:sensitive',
+			Hashtag: 'as:Hashtag',
+		},
+		toot: {
+			Emoji: 'toot:Emoji',
+			featured: 'toot:featured',
+			discoverable: 'toot:discoverable',
+			indexable: 'toot:indexable',
+		},
+		schema: {
+			PropertyValue: 'schema:PropertyValue',
+			value: 'schema:value',
+		},
+		misskey: {
+			'_misskey_content': 'misskey:_misskey_content',
+			'_misskey_quote': 'misskey:_misskey_quote',
+			'_misskey_reaction': 'misskey:_misskey_reaction',
+			'_misskey_votes': 'misskey:_misskey_votes',
+			'isCat': 'misskey:isCat',
+		},
+		fedibird: {
+			quoteUri: 'fedibird:quoteUri',
+			searchableBy: { '@id': 'fedibird:searchableBy', '@type': '@id' },
+		},
+	};
+
+	// flatten defs for processing
+	type Def = {
+		/*** Key, eg: '_misskey_content' */
+		key: string;
+		/*** Resolved value, eg: 'misskey:_misskey_content' */
+		value: string | Object;
+		/*** Required namaspace, eg: 'misskey' */
+		ns: string;
+	};
+
+	const fdefs = new Map<string, Def>();
+
+	for (const [ns, nsdef] of Object.entries(defs)) {
+		for (const [key, value] of Object.entries(nsdef)) {
+			fdefs.set(key, { key, ns, value })
+		}
+	}
+
+	// とりま全部
+	const requiredKeys = Array.from(fdefs.keys());
+
+	// builds
+	const exporseNss = new Map<string, string>();
+	const exposeDefs = new Map<string, string | object>();
+	for (const key of requiredKeys) {
+		const def = fdefs.get(key);
+		if (def == null) {
+			console.warn(`JSON-LD: key=${key} is not in defs, bug?`);
+		} else {
+			const nsValue = nss[def.ns];
+			if (nsValue === undefined) {
+				console.warn(`JSON-LD: ns=${key} is not in nss, bug?`);
+			} else if (nsValue === null) {
+				// alredy in imported one, so safety ignore it
+			} else {
+				exporseNss.set(def.ns, nsValue)
 			}
-		]
+
+			exposeDefs.set(key, def.value)
+ 		}
+	}
+
+	const context = [
+		imports,
+		{
+			...Object.fromEntries(exporseNss),
+			...Object.fromEntries(exposeDefs),
+		}
+	];
+	
+	console.log(JSON.stringify(context));	// ★
+
+	return Object.assign({
+		'@context': context
 	}, x);
 };
 
